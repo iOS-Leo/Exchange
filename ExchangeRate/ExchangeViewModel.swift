8ba38c7a9454903df6e1cbec2c78d9536370a4d9
Leo <gabuevleo991@gmail.com>
2026-05-14 15:43:50 +0300

    feat: add currency table view with custom cell and calculation logic

--- ExchangeRate/ExchangeViewModel.swift
+++ ExchangeRate/ExchangeViewModel.swift
@@ -12,6 +12,7 @@ final class ExchangeViewModel {
     private let service = NetworkService()
     private var allRates: [String: Double] = [:]
     private var currencyCodes: [String] = []
+    private var baseAmountInSystemCurrency: Double = 1.0
     
     func loadData (){
         service.fetch { [weak self] result in
@@ -26,6 +27,26 @@ final class ExchangeViewModel {
                 }
             }
         }
+    }
+    
+    func numberOfRows() -> Int {
+        return currencyCodes.count
+    }
+
+    func getCurrencyCode(at index: Int) -> String {
+        return currencyCodes[index]
+    }
+
+    func getAmount(at index: Int) -> Double {
+        let code = currencyCodes[index]
+        let rate = allRates[code] ?? 1.0
         
+        return baseAmountInSystemCurrency * rate
+    }
+    
+    func updateAmount(newAmount: Double, for currencyCode: String) {
+        let rate = allRates[currencyCode] ?? 1.0
+        baseAmountInSystemCurrency = newAmount / rate
+        delegate?.didFetchCurrencies(allRates)
     }
 }
